d104c3533f70d51b0ac130242832820a83ad579f
Amirsoveizi <amirsoveizi2004@gmail.com>
2023-10-24 22:19:29 +0330

    first commit

---
+++ app/src/main/java/com/exercise/git/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.exercise.git
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun cleartext(view: View) {
+        var txt = this.findViewById<EditText>(R.id.UserName)
+        txt.text.clear()
+    }
+
+    fun showToast(view: View) {
+
+        var txt = this.findViewById<EditText>(R.id.UserName).text
+
+        Toast.makeText(applicationContext,"WelCome ${txt}", Toast.LENGTH_LONG).show()
+    }
+
+}
